808a888c7885576b6d5fd70086df11656af42051
FattahWahyu <fattahillahwahyu14@gmail.com>
2023-11-12 20:45:04 +0700

    fix server

--- src/server.js
+++ src/server.js
@@ -2,20 +2,20 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 9000,
-        host: 'localhost',
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-        },
-    });
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    server.route(routes);
+  server.route(routes);
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();

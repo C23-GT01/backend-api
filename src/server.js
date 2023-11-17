3439cc84270ffcfe74254cfbab2f12100f7b6988
alfimonth <alvy1610@gmail.com>
2023-11-17 07:31:20 +0700

    mendaftarkan plugin upload di server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const path = require('path');
 
 const products = require('./api/products');
 const ProductsService = require('./services/postgres/ProductsService');
 const umkms = require('./api/umkm');
 const UmkmsService = require('./services/postgres/UmkmService');
+
+// uploads
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const UploadsValidator = require('./validator/uploads');
 
 // validator
 const ProductsValidator = require('./validator/products');
@@ -14,6 +20,7 @@ const UmkmsValidator = require('./validator/umkm');
 const init = async () => {
   const productsService = new ProductsService();
   const umkmsService = new UmkmsService();
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -39,6 +46,13 @@ const init = async () => {
         options: {
           service: umkmsService,
           validator: UmkmsValidator,
+        },
+      },
+      {
+        plugin: uploads,
+        options: {
+          service: storageService,
+          validator: UploadsValidator,
         },
       },
     ],

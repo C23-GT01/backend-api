3390d7799fa0e6b9e3be8e1a3df51d7a6c9ed365
alfimonth <alvy1610@gmail.com>
2023-11-17 07:30:12 +0700

    membuat plugin uploads

---
+++ src/api/uploads/routes.js
@@ -0,0 +1,16 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/upload/images',
+    handler: handler.postUploadImageHandler,
+    options: {
+      payload: {
+        allow: 'multipart/form-data',
+        multipart: true,
+        output: 'stream',
+      },
+    },
+  },
+];
+
+module.exports = routes;
